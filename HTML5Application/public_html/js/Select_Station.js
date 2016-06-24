f3c7dae08a3ca6b60ad81084ba7d2ac899266707
Edmund Chow <hoed92@gmail.com>
2016-06-25 04:17:39 +0800

    State change with respect to number of players

--- HTML5Application/public_html/js/Select_Station.js
+++ HTML5Application/public_html/js/Select_Station.js
@@ -5,22 +5,20 @@
  */
 var ref = new Firebase("https://mantrodev.firebaseio.com/STATIONS");
 	var values, dropdown, count, option, selected_val;
-	// Attach an asynchronous callback to read the data at our posts reference
-	ref.on("value", function(snapshot) {
-		values = snapshot.val();
+        
+        var query = ref.orderByChild("station_type").startAt("HOOTHOOT").endAt("HOOTHOOT");
+        query.once("value", function(allStationsSnapshot) {
+            allStationsSnapshot.forEach(function(StationsSnapshot) {
+                values = allStationsSnapshot.val();
 		count = Object.keys(values).length;
                 dropdown = document.getElementById("stn_list");
 		for (i = 0; i < count; i++) {
                     option = document.createElement("option");
-                    option.text = Object.keys(values)[i];
+                    option.text = StationsSnapshot.key();
                     dropdown.appendChild(option);
-                    console.log(Object.keys(values)[i]);
 		}
-                
-                console.log("Value selected is: " + dropdown.options[dropdown.selectedIndex].value);
                 selected_val = dropdown.options[dropdown.selectedIndex].value;
-	}, function (errorObject) {
-	  console.log("The read failed: " + errorObject.code);
-	});        
+            });
+        });
 
 
--- HTML5Application/public_html/js/waiting_for_player.js
+++ HTML5Application/public_html/js/waiting_for_player.js
@@ -13,64 +13,94 @@ window.onload = function(){
     document.getElementById("stn_name").innerHTML = station_name;
 };
 
-// Update state of station to "waiting_for_players"
+// Update state of station to "waiting_for_players" when entering 
+// waiting_for_players.html for the first time
 var ref = new Firebase("https://mantrodev.firebaseio.com/STATIONS/" + station_name);
-var state_ref = ref.child("state");
-state_ref.update({
+ref.update({
     "state": "waiting_for_players" 
 });
+ref.on("value", function(snapshot) {
+    var state_val = snapshot.val();
+    var p = document.getElementById("state_ref");
+    p.textContent = state_val.state;
+});
 
-// Retrieve, display player nicknames & picture of specific station
+// Retrieve & display player list
 ref = new Firebase("https://mantrodev.firebaseio.com/STATIONS/" + station_name + "/PLAYERS");
-        var newPlayer, editedPlayer, deletedPlayer, player_count, list, pname, text_li, user_icon;
-        
+    var existingPlayer, newPlayer, editedPlayer, deletedPlayer, player_count, list, pname, text_li, user_icon, values;
+
+    // Retrieve & display new players as they are added to firebase
+    ref.on("child_added", function(snapshot) {
+        // Extract out player nickname
+        newPlayer = snapshot.val();
+
+        list = document.createElement("li");                        // Create a <li> element
+        list.id = snapshot.key() + "";                              // Tag the id to li element
+
+        var div = document.createElement("div");                    // Create <div> element
+        user_icon = document.createElement("img");                  // Create <img> element
+        user_icon.width = 50;                                       // Set picture width to 50
+        user_icon.src = newPlayer.icon_url;                         // Set picture source to icon_url obtained from firebase
+        div.appendChild(user_icon);                                 // Append user_icon to <div> element
+
+        pname = newPlayer.nickname;
+        text_li = document.createTextNode(pname);                   // Create a text node
+        div.appendChild(text_li);                                   // Append the text to <li>
+
+        list.appendChild(div);                                      // Append <div> element to <li> element
+        document.getElementById("player_name").appendChild(list);   // Append <li> to <ul> with id="player_name"
+
         // Update state of station to "starting" when player count > 0
+        ref = new Firebase("https://mantrodev.firebaseio.com/STATIONS/" + station_name + "/PLAYERS");
         ref.on("value", function(snapshot) {
             values = snapshot.val();
             player_count = Object.keys(values).length;
+            ref = new Firebase("https://mantrodev.firebaseio.com/STATIONS/" + station_name);
             if (player_count >= 1){
-                ref = new Firebase("https://mantrodev.firebaseio.com/STATIONS/" + station_name);
-                var state_ref = ref.child("state");
-                state_ref.update({
+                ref.update({
                     "state": "starting" 
+                });
+                ref.on("value", function(snapshot) {
+                    var state_val = snapshot.val();
+                    var p = document.getElementById("state_ref");
+                    p.textContent = state_val.state;
                 });
             }
         });
-            
-        // Retrieve & display new players as they are added to firebase
-        ref.on("child_added", function(snapshot, prevChildKey) {
-            // Extract out player nickname
-            newPlayer = snapshot.val();
-            pname = newPlayer.nickname;
-            
-            list = document.createElement("li");                        // Create a <li> element
-            list.id = snapshot.key() + "";                              // Tag the id to li element
-            
-            var div = document.createElement("div");                    // Create <div> element
-            user_icon = document.createElement("img");                  // Create <img> element
-            user_icon.width = 50;                                       // Set picture width to 50
-            user_icon.src = newPlayer.animal_icon.icon_url;             // Set picture source to icon_url obtained from firebase
-            div.appendChild(user_icon);                                 // Append user_icon to <div> element
-            
-            text_li = document.createTextNode(pname);                   // Create a text node
-            div.appendChild(text_li);                                   // Append the text to <li>
-            
-            list.appendChild(div);                                      // Append <div> element to <li> element
-            document.getElementById("player_name").appendChild(list);   // Append <li> to <ul> with id="player_name"
-        });
-        
-        // Update list of players after deletion
-        ref.on("child_removed", function(snapshot) {
-            // Snapshot of the deleted player values
-            deletedPlayer = snapshot.val();
-            
-            // Remove player from list when deleted in firebase
-            var ul = document.getElementById("player_name");
-            var items = ul.getElementsByTagName("li");
-            var k;
-            for (k = 0; k < items.length; k++){
-                if (snapshot.key() === ul.childNodes[k].id){
-                   ul.removeChild(ul.childNodes[k]);
-                }
+    });
+
+    // Update list of players after deletion
+    ref.on("child_removed", function(snapshot) {
+        // Snapshot of the deleted player values
+        deletedPlayer = snapshot.val();
+
+        // Remove player from list when deleted in firebase
+        var ul = document.getElementById("player_name");
+        var items = ul.getElementsByTagName("li");
+        var k;
+        for (k = 0; k < items.length; k++){
+            if (snapshot.key() === ul.childNodes[k].id){
+               ul.removeChild(ul.childNodes[k]);
+            }
+        }
+        // Update state of station to "waiting_for_players" when player count < 1
+        ref = new Firebase("https://mantrodev.firebaseio.com/STATIONS/" + station_name + "/PLAYERS");
+        ref.on("value", function(snapshot) {
+            values = snapshot.val();
+            player_count = Object.keys(values).length;
+            ref = new Firebase("https://mantrodev.firebaseio.com/STATIONS/" + station_name);
+            if (player_count < 1){
+                ref.update({
+                    "state": "waiting_for_players" 
+                });
+                ref.on("value", function(snapshot) {
+                    var state_val = snapshot.val();
+                    var p = document.getElementById("state_ref");
+                    p.textContent = state_val.state;
+                });
             }
         });
+    });
+        
+        
+             
