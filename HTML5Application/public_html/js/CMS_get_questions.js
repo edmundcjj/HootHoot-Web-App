815ec7b3d806ea361f462ffd0622ee7e5b4fdaea
Edmund Chow <hoed92@gmail.com>
2016-06-28 03:54:13 +0800

    Updated codes for questions and options retrieval from CMS

--- HTML5Application/public_html/js/CMS_get_questions.js
+++ HTML5Application/public_html/js/CMS_get_questions.js
@@ -3,20 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+        
 
+// Retrieve all questions from CMS and save it to QNS_BANK array
 var xmlhttp = new XMLHttpRequest();
 var url = 'http://hootsq-mantro.azurewebsites.net/api/Questions/GetQuestions';
 var questionBank = [];
 xmlhttp.onreadystatechange = function() {
-        console.log("Result Ready");
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
                 questionBank = JSON.parse(xmlhttp.responseText);
                 if(questionBank.length > 0)
                 {
-                        console.log(questionBank);
+                    console.log(questionBank);
                 }
         }
-
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
+    
+
